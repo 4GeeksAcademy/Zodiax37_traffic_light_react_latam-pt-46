547a282990877e77fc1e75d5783515ba41dacf3c
Zodiax37 <98854282+Zodiax37@users.noreply.github.com>
2025-05-07 05:45:47 +0000

    First Commit

---
+++ src/js/components/Semaforo.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import '../../styles/styleTrafficLight.css'
+
+export const Semaforo = () => {
+    
+    const [Color, setColor] = useState("red")
+
+    const selectLight = (e)=>{
+
+        document.querySelectorAll('.Light').forEach(light => {
+            light.classList.remove('selected');
+        });
+
+        e.target.classList.add('selected');
+
+        setColor(e.target.classList[0]);
+    }
+
+    return (
+        <div className='mt-5'>
+            <div className='semaforo'>
+                <div className='red Light' onClick={selectLight}></div>
+
+                <div className='yellow Light' onClick={selectLight}></div>
+
+                <div className='green Light' onClick={selectLight}></div>
+            </div>
+        </div>
+    )
+}
